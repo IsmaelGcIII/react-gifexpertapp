c906da76a024fa566a19f14c5cdae2e28350a35c
Ismael Castillo <ismael360_live@hotmail.com>
2021-07-22 20:34:43 -0500

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+   
+    const [categories,setCategories] = useState(['Shingeki no kyojin']);
+
+   // const handleAdd = () =>{
+     //setCategories(['hunterXhunter',...categories]);
+     //  setCategories(cats =>[...cats,'HunterxHunter']);
+
+        
+ //   }
+
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+            <hr />
+
+
+            <ol>
+                {
+
+                    categories.map ( category => (
+                         <GifGrid 
+                         key = {category}
+                         category= {category} />
+                    ))
+
+                }  
+            </ol>
+
+
+        </>
+    )
+}
+
